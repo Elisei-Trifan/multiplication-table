ed8a337d59e49937cd1361c2506532e76e9aefd5
Elisei-Trifan <slavaberzan@gmail.com>
2024-07-31 11:37:43 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const elemInput = document.querySelector('.js-input')
+const elemButton = document.querySelector('.js-button')
+const elemOutput = document.querySelector('.js-output')
+const elemGreet = document.querySelector('.greetings')
+
+
+const question = prompt('Введи свое имя', 'Имя')
+if (question === 'Елисей' || question === 'елисей') {
+    elemGreet.innerHTML = 'Привет дорогой Елисей'
+}
+
+elemButton.addEventListener('click', function() {
+    let asd = []
+    for (let i = 2; i <= 9; i++) {
+        if (+   elemInput.value < 0) {
+            return elemOutput.innerHTML = 'Это отрицательное значение'
+        }
+        else if (+elemInput.value === 0) {
+            return elemOutput.innerHTML = 'Это ноль, введите положительное число'
+        }  
+        else if (+elemInput.value > 10) {
+            return elemOutput.innerHTML = 'Число больше 10'
+        }
+         else {
+            asd.push(+elemInput.value + ' * ' + i + ' = ' +elemInput.value*i)
+        }
+     }
+    return elemOutput.innerHTML = asd.join('<br>')
+   
+})
