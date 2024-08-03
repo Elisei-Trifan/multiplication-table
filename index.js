98a71d75363fdfdf654b2ab2ea88e249ed274705
Elisei-Trifan <slavaberzan@gmail.com>
2024-08-03 16:45:00 +0300

    change link

--- index.js
+++ index.js
@@ -9,17 +9,17 @@ const elemFoto = document.querySelector('.foto')
 const question = prompt('Введи свое имя', 'Имя')
     name = question.trim()
     if (name === 'Рувим') {
-        elemFoto.innerHTML = `<img src="/img/photo_2024-08-03_15-02-49.jpg" alt="фотка Рувима"></img>`
+        elemFoto.innerHTML = `<img src="img/photo_2024-08-03_15-02-49.jpg" alt="фотка Рувима"></img>`
     } else if (name === 'Елисей') {
-        elemFoto.innerHTML = `<img src="/img/20170717_1439543.png" alt="фотка Елисея"></img>`
+        elemFoto.innerHTML = `<img src="img/20170717_1439543.png" alt="фотка Елисея"></img>`
     } else if (name === 'Виталик') {
-        elemFoto.innerHTML = `<img src="/img/20170717_143953.jpg" alt="фотка Виталика"></img>`
+        elemFoto.innerHTML = `<img src="img/20170717_143953.jpg" alt="фотка Виталика"></img>`
     } else if (name === 'Вера') {
-        elemFoto.innerHTML = `<img src="/img/20170711_164844.jpg" alt="фотка Веры"></img>`
+        elemFoto.innerHTML = `<img src="img/20170711_164844.jpg" alt="фотка Веры"></img>`
     } else if (name === 'Алина') {
-        elemFoto.innerHTML = `<img src="/img/20170711_154742.png" alt="фотка Алины"></img>`
+        elemFoto.innerHTML = `<img src="img/20170711_154742.png" alt="фотка Алины"></img>`
     }  else if (name === 'Надя') {
-        elemFoto.innerHTML = `<img src="/img/20170711_143939.jpg" alt="фотка Нади"></img>`
+        elemFoto.innerHTML = `<img src="img/20170711_143939.jpg" alt="фотка Нади"></img>`
     }
      elemGreet.innerHTML = `Привет  ${name}`
 
